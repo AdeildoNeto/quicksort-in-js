8f18d2b632f176f1ab32d7ddf63a719c43fd4e32
AdeildoNeto <n.santos1897@gmail.com>
2020-06-22 21:53:55 -0300

    DONE

---
+++ quicksort.js
@@ -0,0 +1,36 @@
+function quicksort(array) {
+if(array.length === 0) return [];
+if(array.length === 1) return array;
+
+ let pivot = array[0];
+
+ let {smallers, equal, biggers} = spread(array, pivot);
+
+ return quicksort(smallers).concat(equal).concat(quicksort(biggers));
+
+}
+
+function spread(arr, pivot) {
+    let smallers = [];
+    let equal = [];
+    let biggers = [];
+
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] < pivot) {
+            smallers.push(arr[i])
+        }
+        if(arr[i] == pivot) {
+            equal.push(arr[i]);
+        }
+        if(arr[i] > pivot) {
+            biggers.push(arr[i])
+        }
+    }
+    return {
+        smallers, equal, biggers
+    }
+}
+
+console.log(quicksort([3, 44, 5,7, 6, 2, 7]));
+
+
